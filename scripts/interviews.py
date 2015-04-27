186d6dc26572a80fc75002bf78fd19e58f9224c8
scott <scott@cogolabs.com>
2015-04-27 09:43:21 -0400

    tower of hanoi in interviews.py

--- scripts/interviews.py
+++ scripts/interviews.py
@@ -86,3 +86,49 @@ def euclids_recurse(a, b):
         return a
     else:
         return euclids_recurse(b, a % b)
+
+
+###### TOWER OF HANOI ########
+class Pole(object):
+    def __init__(self, num_rings, name=""):
+        self.letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P']
+        self.rings = self.letters[:num_rings]
+        self.name = name
+        self.num_rings = len(self.rings)
+
+    def __str__(self):
+        return self.name + ' [' + ', '.join(item for item in self.rings) + ']'
+
+
+class Tower(object):
+    def __init__(self, num_rings=1):
+        self.from_pole = Pole(num_rings, 'from_pole')
+        self.to_pole = Pole(0, 'to_pole')
+        self.with_pole = Pole(0, 'with_pole')
+        self.total_moves = 0
+
+    def __str__(self):
+        return str(self.from_pole) + '\n' + str(self.with_pole) + '\n' + str(self.to_pole)
+
+    def move_tower(self, num_rings, from_pole, to_pole, with_pole):
+        if num_rings < 2:
+            self.move_disk(from_pole, to_pole)
+        else:
+            self.move_tower(num_rings - 1, from_pole, with_pole, to_pole)
+            self.move_disk(from_pole, to_pole)
+            self.move_tower(num_rings - 1, with_pole, to_pole, from_pole)
+
+    def move_disk(self, fp, tp):
+        print 'Moving disk from ' + str(fp) + ' to ' + str(tp)
+        d = fp.rings.pop()
+        tp.rings.append(d)
+        print self
+        self.total_moves += 1
+
+
+
+if __name__ == '__main__':
+    tow = Tower(4)
+    print tow
+    tow.move_tower(tow.from_pole.num_rings, tow.from_pole, tow.to_pole, tow.with_pole)
+    print 'Total moves: ', tow.total_moves
