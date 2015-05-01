42d759553fa9b1db2b66b86414e958b09bb55a76
scott <scott@cogolabs.com>
2015-05-01 13:09:55 -0700

    gen work

--- scripts/generators.py
+++ scripts/generators.py
@@ -16,16 +16,16 @@ def gen_perms(a):
                 yield a[i:i+i] + perm
 
 
-def generate_weights(weights, poss_weights):
-    '''generator for an array of length n
-    of all possible weights from -1 to 1
-        weights: array of numbers, each slot has a set to choose from
-        poss_weights: the set of weights to choose from
-        cur_idx: the current indexo f the array that you are changing
-        max_idx: the highest index that will change'''
-    while max_idx < len(weights):
-        yield weights
-        weights[cur_idx] = poss_weights[cur_idx]
+# def generate_weights(weights, poss_weights):
+#     '''generator for an array of length n
+#     of all possible weights from -1 to 1
+#         weights: array of numbers, each slot has a set to choose from
+#         poss_weights: the set of weights to choose from
+#         cur_idx: the current indexo f the array that you are changing
+#         max_idx: the highest index that will change'''
+#     while max_idx < len(weights):
+#         yield weights
+#         weights[cur_idx] = poss_weights[cur_idx]
 
 
 def main():
@@ -36,10 +36,10 @@ def main():
     # to create for the weight vector
     weights = [poss_weights[0] for i in range(n)]
     print weights
-    g = generate_weights(weights, 0)
-    print list(g)
-    f = fib()
-    print list(f)
+    # g = generate_weights(weights, 0)
+    # print list(g)
+    # f = fib()
+    # print list(f)
 
     p = permute_string('abcd')
     while True:
