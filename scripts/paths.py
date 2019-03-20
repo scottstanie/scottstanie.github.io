f309745193a92146b0dd9d404ad516cc9692c879
scott <scott.stanie@gmail.com>
2019-03-20 10:13:15 -0500

    a star algo

--- scripts/paths.py
+++ scripts/paths.py
@@ -35,6 +35,52 @@ def dijkstra(graph, src, dest):
     return final_costs
 
 
+def a_star(graph, src, dest, idx_lookup):
+    """Find shortest path from src to dest with heuristics
+
+    graph:
+        dict[list[(int, int)]]: {node: [adj_node1, adj_node2,...]}
+        src (int):
+        dest (int):
+    """
+
+    dest_idx = idx_lookup[dest]
+
+    def heuristic(test_idx, lnorm=np.inf):
+        if lnorm == 1:
+            return sum((abs(test_idx[0] - dest_idx[0]), abs(test_idx[1] - dest_idx[1])))
+        elif lnorm == 2:
+            return np.sqrt((test_idx[0] - dest_idx[0])**2 + (test_idx[1] - dest_idx[1])**2)
+        else:
+            # L-inf norm (chessboard distance) to the destination
+            return max((abs(test_idx[0] - dest_idx[0]), abs(test_idx[1] - dest_idx[1])))
+
+    to_process = PriorityQueue()
+    to_process.put((0, 0, src))
+
+    final_costs = {}
+    while to_process.qsize() > 0:
+        # print(list(to_process.queue))
+        cur_heur_cost, cur_cost, cur_node = to_process.get()
+        if cur_node in final_costs:
+            continue
+
+        # import ipdb
+        # ipdb.set_trace()
+        for weight_cur_next, next_node in graph[cur_node]:
+            test_idx = idx_lookup[next_node]
+            heur_cost = heuristic(test_idx)
+            to_process.put((
+                heur_cost + cur_cost + weight_cur_next,
+                cur_cost + weight_cur_next,
+                next_node,
+            ))
+
+        final_costs[cur_node] = cur_cost
+
+    return final_costs
+
+
 def build1(n):
     graph = defaultdict(list)
     for i in range(1, n + 1):
@@ -46,11 +92,15 @@ def build1(n):
     return graph
 
 
-def build_grid(n):
+def build_grid(n, neighbors=8):
     def neighbors8(point):
         x, y = point
         return ((x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1), (x + 1, y + 1), (x - 1, y - 1),
                 (x + 1, y - 1), (x - 1, y + 1))
+
+    def neighbors4(point):
+        x, y = point
+        return ((x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1))
 
     node_grid = np.arange(n**2).reshape((n, n))
     node_grid = np.pad(node_grid, (1, 1), mode='constant', constant_values=-1)
@@ -60,7 +110,8 @@ def build_grid(n):
     for idx in range(1, n + 1):
         for jdx in range(1, n + 1):
             cur = node_grid[idx, jdx]
-            neighbors = [node_grid[n] for n in neighbors8((idx, jdx))]
+            neigh_idxs = neighbors4((idx, jdx)) if neighbors == 4 else neighbors8((idx, jdx))
+            neighbors = [node_grid[n] for n in neigh_idxs]
             graph[cur] = [(1, n) for n in neighbors if n >= 0]
 
             idx_lookup[cur] = (idx, jdx)
@@ -79,7 +130,12 @@ def plot_grid_costs(costs, node_grid, idx_lookup):
     plt.show(block=True)
 
 
-def animate_costs(costs, node_grid, idx_lookup, interval=200, outname='dijkstra.gif'):
+def animate_costs(costs,
+                  node_grid,
+                  idx_lookup,
+                  interval=200,
+                  algo="Dijkstra's algorithm",
+                  outname='dijkstra.gif'):
     fig, ax = plt.subplots(1, 1)
     fig.set_size_inches((4, 4))
     fig.tight_layout()
@@ -88,7 +144,7 @@ def animate_costs(costs, node_grid, idx_lookup, interval=200, outname='dijkstra.
     max_cost = max(costs.values())
     axim = ax.imshow(cost_grid, vmax=max_cost, cmap='jet')
     fig.colorbar(axim)
-    ax.set_title("Path explored by Dijkstra's algorithm")
+    ax.set_title("Path explored by %s" % algo)
     cost_items = list(costs.items())  # Should preserve order of adding (python3.5)
 
     def update_im(idx):
@@ -117,9 +173,22 @@ if __name__ == '__main__':
     src = 0
     dest = n**2 - 1
     costs = dijkstra(graph, src, dest)
-    print("Cost from %s to %s" % (src, dest))
-    # print(costs[dest])
-    print(sorted(costs.items()))
+    print("Dijkstra cost from %s to %s" % (src, dest))
+    print(costs[dest])
+    # print(sorted(costs.items()))
+
+    costs = a_star(graph, src, dest, idx_lookup)
+    print("A* Cost from %s to %s" % (src, dest))
+    print(costs[dest])
+    # print(sorted(costs.items()))
 
     # plot_grid_costs(costs, node_grid, idx_lookup)
-    animate_costs(costs, node_grid, idx_lookup, interval=100, outname='dijkstra.gif')
+    animate_costs(
+        costs,
+        node_grid,
+        idx_lookup,
+        interval=100,
+        outname='a_star.gif',
+        algo="A*"
+        # outname=None,
+    )
